241a6ce5e0085b5c77d9a7615cdf491fad7c8571
Kristoffer Masajo <kcmasajo@yahoo.com>
2023-03-30 02:49:09 +0800

    4.4 helper functions and unit tests, step2

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -6,3 +6,37 @@ test('dummy returns one', () => {
   const result = listHelper.dummy(blogs);
   expect(result).toBe(1);
 });
+
+describe('total likes', () => {
+  test('of empty list is zero', () => {
+    const blogs = [];
+
+    const result = listHelper.totalLikes(blogs);
+    expect(result).toBe(0);
+  });
+
+  test('when list has only one blog equals the likes of that', () => {
+    const blogs = [
+      {
+        title: "Title",
+        author: "Author",
+        url: "URL",
+        likes: 5
+      }
+    ];
+
+    const result = listHelper.totalLikes(blogs);
+    expect(result).toBe(blogs[0].likes);
+  });
+
+  test('of a bigger list is calculated right', () => {
+    const blogs = [
+      { title: "A", author: "A", url: "www", likes: 1 },
+      { title: "B", author: "B", url: "www", likes: 1 },
+      { title: "C", author: "C", url: "www", likes: 1 }
+    ];
+
+    const result = listHelper.totalLikes(blogs);
+    expect(result).toBe(3);
+  });
+});
