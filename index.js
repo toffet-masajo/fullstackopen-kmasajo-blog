8673e8378e3984bf907013a009a6b4f500bab505
Kristoffer Masajo <kcmasajo@yahoo.com>
2023-03-30 01:18:30 +0800

    Step2.1: Create utils/logger

--- index.js
+++ index.js
@@ -1,42 +1,44 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+const logger = require('./utils/logger');
+
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
 
 const blogSchema = new mongoose.Schema({
   title: String,
   author: String,
   url: String,
   likes: Number
-})
+});
 
-const Blog = mongoose.model('Blog', blogSchema)
+const Blog = mongoose.model('Blog', blogSchema);
 
-const mongoUrl = 'mongodb+srv://fullstack:fullstack123@fullstackopen.vrpdwcv.mongodb.net/?retryWrites=true&w=majority'
-mongoose.connect(mongoUrl)
+const mongoUrl = 'mongodb+srv://fullstack:fullstack123@fullstackopen.vrpdwcv.mongodb.net/?retryWrites=true&w=majority';
+mongoose.connect(mongoUrl);
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 app.get('/api/blogs', (request, response) => {
   Blog
     .find({})
     .then(blogs => {
-      response.json(blogs)
-    })
-})
+      response.json(blogs);
+    });
+});
 
 app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
+  const blog = new Blog(request.body);
 
   blog
     .save()
     .then(result => {
-      response.status(201).json(result)
-    })
-})
+      response.status(201).json(result);
+    });
+});
 
-const PORT = 3003
+const PORT = 3003;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  logger.info(`Server running on port ${PORT}`);
+});
