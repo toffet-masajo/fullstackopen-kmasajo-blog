db152c0cd129acd83452d333e632d429c38063c6
Kristoffer Masajo <kcmasajo@yahoo.com>
2023-03-30 01:55:59 +0800

    4.2 Blog list, step2

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
 
-const Blog = require('./models/blogs');
+const blogRouter = require('./controllers/blogs');
 const config = require('./utils/config');
 const logger = require('./utils/logger');
 const middleware = require('./utils/middleware');
@@ -16,23 +16,7 @@ mongoose.connect(config.MONGODB_URL)
 app.use(cors());
 app.use(express.json());
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs);
-    });
-});
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result);
-    });
-});
+app.use('/api/blogs', blogRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
