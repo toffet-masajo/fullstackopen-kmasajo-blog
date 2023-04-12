fbcd207adeb1e8cf34284220ec0eece7f0796a9c
Kristoffer Masajo <toffet-masajo@users.noreply.github.com>
2023-04-13 02:27:40 +0800

    4.15 bloglist expansion, step3

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 
 const blogRouter = require('./controllers/blogs');
+const userRouter = require('./controllers/users');
 const config = require('./utils/config');
 const logger = require('./utils/logger');
 const middleware = require('./utils/middleware');
@@ -17,6 +18,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/blogs', blogRouter);
+app.use('/api/users', userRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
