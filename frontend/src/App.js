bace989ab8b76d56afe3ae1fa75b02d0aadf35b7
Kristoffer Masajo <toffet-masajo@users.noreply.github.com>
2023-04-29 01:52:45 +0800

    5.4: bloglist frontend, step4

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ const App = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [user, setUser] = useState(null);
+  const [message, setMessage] = useState({});
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -24,6 +25,17 @@ const App = () => {
     }
   }, []);
 
+  const messageForm = () => {
+    if( message === null ) return null;
+    if( message.type === 'ok' )
+      return (
+        <div><h2 style={{ color: 'green' }}>{ message.message }</h2></div>
+      );
+    return (
+      <div><h2 style={{ color: 'red' }}>{ message.message }</h2></div>
+    );
+  };
+
   const handleLogin = async (event) => {
     event.preventDefault();
 
@@ -36,9 +48,9 @@ const App = () => {
       setUser(loggedUser);
       setUsername('');
       setPassword('');
-
     } catch (error) {
-
+      setMessage({ message: 'wrong username or password', type: 'ng' });
+      setTimeout( () => setMessage(null), 5000);
     }
   };
 
@@ -52,6 +64,7 @@ const App = () => {
     return( 
       <div>
         <h2>Log in to application</h2>
+        { message && messageForm() }
         <form onSubmit={handleLogin}>
           <div>
             username <input type="text" value={username} name="Username" onChange={({ target }) => setUsername(target.value)} />
@@ -73,8 +86,22 @@ const App = () => {
       url: event.target.url.value
     };
 
-    const data = await blogService.createBlog(newBlog);
-    setBlogs(blogs.concat(data));
+    event.target.title.value = '';
+    event.target.author.value = '';
+    event.target.url.value = '';
+
+    try {
+      const data = await blogService.createBlog(newBlog);
+      setBlogs(blogs.concat(data));
+      setMessage({ 
+        message: `a new blog ${newBlog.title} by ${newBlog.author} added`, 
+        type: 'ok' 
+      });
+    } catch (error) {
+      setMessage({ message: 'error adding blog', type: 'ng'});
+    } finally {
+      setTimeout( () => setMessage(null), 5000 );
+    }
   };
 
   const newForm = () => {
@@ -97,6 +124,7 @@ const App = () => {
     return(
       <div>
         <h2>Blogs</h2>
+        { message && messageForm() }
         <p>{user.name} logged in <button onClick={handleLogout}>logout</button></p>
         {newForm()}
         {blogs.map(blog =>
