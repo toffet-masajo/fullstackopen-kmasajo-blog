ed2d0b2e3c8141789d63b55318dbdef5f8a9ff43
Kristoffer Masajo <toffet-masajo@users.noreply.github.com>
2023-05-09 02:37:10 +0800

    5.7: bloglist frontend, step7

--- frontend/src/components/Blog.js
+++ frontend/src/components/Blog.js
@@ -1,7 +1,26 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-);
+import { useState } from "react";
+
+const Blog = ({blog}) => {
+  const [visible, setVisible] = useState(false);
+  
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author} <button onClick={() => setVisible(!visible)}>{visible ? 'hide' : 'view'}</button>
+      <div style={{ display : visible ? '' : 'none' }}>
+        {blog.url}<br />
+        {blog.likes} <button>like</button><br />
+        {blog.user.name}<br />
+      </div>
+    </div>
+  );
+};
 
 export default Blog;
